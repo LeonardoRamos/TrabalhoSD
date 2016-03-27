3e58139708bca2ee2a567a4e1ca4fd251aeeab44
LeonardoRamos <leoslramos@gmail.com>
2016-03-27 15:37:25 -0300

    Versao final do programa

--- readerWriterMPI.c
+++ readerWriterMPI.c
@@ -24,9 +24,8 @@
 #define MAXPROC 1000
 
 main (int argc, char **argv) {
-	int readerIndex = 0, writerIndex = 0, readerStartIndex = 0, writerStartIndex = 0,
-		totalWritersInQueue = 0, totalReadersInQueue = 0, numberOfReaders, numberOfWriters, 
-	    totalProcesses, processId, readerCount = 0, isWriting = 0, data_transfer_tag = 1;
+	int readerStartIndex = 0, writerStartIndex = 0, totalWritersInQueue = 0, totalReadersInQueue = 0, nextWriterIndex = 0, nextReaderIndex = 0,
+		numberOfReaders, numberOfWriters, totalProcesses, processId, readerCount = 0, isWriting = 0, data_transfer_tag = 1;
 
 	int writerQueue[MAXPROC];
 	int readerQueue[MAXPROC];
@@ -51,13 +50,15 @@ main (int argc, char **argv) {
 
 			if (message[0] == 'R' && message[1] == 'R') { // mensagem de leitor requisitando leitura (RR = RequestRead)
 				if (isWriting == 0) { // envia OK para o processo leitor, área crítica está apta para uso
+					printf("\nRequisição de leitura aceita, processo de id %d irá ler.", sourceProcess); 
 					MPI_Send(&sourceProcess, 1, MPI_INT, sourceProcess, data_transfer_tag, MPI_COMM_WORLD);
 					readerCount++;
 				}
 				else { // adciona na fila, área crítica está ocupada com escrita
+					printf("\nJá há um processo escrevendo e processo de id %d será adicionado a fila de leitores.", sourceProcess); 
+					readerQueue[nextReaderIndex] = sourceProcess;
 					totalReadersInQueue++;
-					readerQueue[readerIndex] = sourceProcess;
-					readerIndex++;
+					nextReaderIndex++;
 				}
 
 			}
@@ -66,59 +67,68 @@ main (int argc, char **argv) {
 
 				// não há mais processos efetuando a leitura, primeiro escritor da fila pode escrever, é enviado OK
 				if (readerCount == 0 && totalWritersInQueue > 0) {
+					printf("\nRequisição de escrita aceita, processo de id %d, que está no início da fila irá escrever.", writerQueue[writerStartIndex]); 
 					MPI_Send(&writerQueue[writerStartIndex], 1, MPI_INT, writerQueue[writerStartIndex], data_transfer_tag, MPI_COMM_WORLD);
 					isWriting = 1;
 					// remove writer da fila
-					writerStartIndex++;
 					writerQueue[writerStartIndex] = 0;
+					writerStartIndex++;
 					totalWritersInQueue--;
 
 					if (totalWritersInQueue == 0) {
+						printf("\nFila de escritores vazia."); 
 						writerStartIndex = 0;
-						writerIndex = 0;
+						nextWriterIndex = 0;
 					}
 				}
 			}
 			else if (message[0] == 'R' && message[1] == 'W') { // mensagem de escritor requisitando escrita (RW = RequestWriter)
 				if (readerCount == 0 && isWriting == 0) {
+					printf("\nRequisição de escrita aceita, processo de id %d irá escrever.", sourceProcess); 
 					MPI_Send(&sourceProcess, 1, MPI_INT, sourceProcess, data_transfer_tag, MPI_COMM_WORLD);
 					isWriting = 0;
 				}
 				else {
+					printf("\nJá há um ou mais processos lendo e processo de id %d será adicionado a fila de escritores.", sourceProcess); 
+					writerQueue[nextWriterIndex] = sourceProcess;
 					totalWritersInQueue++;
-					writerQueue[writerIndex] = sourceProcess;
-					writerIndex++;
+					nextWriterIndex++;
 				}
 			}
-			else if (message[0] == 'E' && message[1] == 'R') {  // mensagem de escritor sinalizando que não está mais escrevendo (EW = EndWriter)
+			else if (message[0] == 'E' && message[1] == 'W') {  // mensagem de escritor sinalizando que não está mais escrevendo (EW = EndWriter)
 				isWriting = 0;
 
-				if (totalReadersInQueue == 0 && totalWritersInQueue > 0) { // terminou de escrever mas ainda tem processo querendo escrever
+				if (totalReadersInQueue == 0 && totalWritersInQueue > 0) { // terminou de escrever mas ainda tem processo querendo ler
+					printf("\nTérmino de escrita, processo de id %d no início da fila de escritores irá escrever.", writerQueue[writerStartIndex]);  
 					MPI_Send(&writerQueue[writerStartIndex], 1, MPI_INT, writerQueue[writerStartIndex], data_transfer_tag, MPI_COMM_WORLD);
 					isWriting = 1;
 					// remove writer da fila
-					writerStartIndex++;
 					writerQueue[writerStartIndex] = 0;
+					writerStartIndex++;
 					totalWritersInQueue--;
 
 					if (totalWritersInQueue == 0) {
+						printf("\nFila de escritores vazia."); 
 						writerStartIndex = 0;
-						writerIndex = 0;
+						nextWriterIndex = 0;
 					}
 				}
 				else { // não há mais processos querendo escrever, todos os processos de leitura serão atendidos
 					while (totalReadersInQueue > 0) {
+						printf("\nNão há mais processos escrevendo, processo de id %d no topo da fila de leitores irá ler.", readerQueue[readerStartIndex]);  
 						MPI_Send(&readerQueue[readerStartIndex], 1, MPI_INT, readerQueue[readerStartIndex], data_transfer_tag, MPI_COMM_WORLD);
 						readerCount++;
 
 						// remove reader da fila
-						readerStartIndex++;
+						printf("\nProcesso de id %d removido da fila de leitores.", readerQueue[readerStartIndex]);
 						readerQueue[readerStartIndex] = 0;
+						readerStartIndex++;
 						totalReadersInQueue--;
 
 						if (totalReadersInQueue == 0) {
+							printf("\nFila de leitores vazia."); 
 							readerStartIndex = 0;
-							readerIndex = 0;
+							nextReaderIndex = 0;
 						}
 					}
 				}
